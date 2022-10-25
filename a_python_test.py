f10c06f1cd75583bf5a7cb58d2b245c6af39891d
eduardorz <ruizzmarin20@gmail.com>
2022-10-25 13:08:08 -0300

    a new change in the python file

--- a_python_test.py
+++ a_python_test.py
@@ -3,5 +3,10 @@
 number3 = 5
 number4 = 8 
 
-productoria = number3 * number4
-print "the product between both numbers are #{productoria}" 
+product = number3 * number4
+print "the product between both numbers are #{product}" 
+
+number5 = 10 
+
+adition = number5 + number4 
+print "the adition between both numbers are #{adition}"
